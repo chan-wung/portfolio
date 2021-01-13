725591d570d6812d975877a1b20772864d592576
chanwung <fhou4444@naver.com>
2021-01-13 20:46:25 +0900

    portfolio: Touch

--- main.js
+++ main.js
@@ -4,13 +4,26 @@
 
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-
+const arrowBtn = document.querySelector(".arrow_btn");
 document.addEventListener('scroll', () => {
     if(window.scrollY > navbarHeight){
         navbar.classList.add('sticky');
+        arrowBtn.classList.add('sticky');
     } else{
         navbar.classList.remove('sticky');
+        arrowBtn.classList.remove('sticky');
     }
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar_toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Show "arrow" button when scrolling down
+arrowBtn.addEventListener('click', (event) => {
+    scrollIntoView('#home');
 });
 
 // Handle scrolling when tapping on the navbar menu
@@ -23,8 +36,12 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null){
         return;
     }
+    navbarMenu.classList.remove('open'); 
     scrollIntoView(link);
-
+    
+    // target.classList.remove('active');
+    // target.classList.add('active');
+    console.log(target);
 });
 
 // Handle click on "contact me" button on home
@@ -39,11 +56,50 @@ const home = document.querySelector(".home_container");
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(1 - window.scrollY / homeHeight);
+    // console.log(1 - window.scrollY / homeHeight);
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Projects
+
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null){
+        return;
+    }
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category_btn.active');
+    active.classList.remove('active');
+    const target = 
+        e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('active');
+    
+
+    // console.log(filter);
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else{
+                project.classList.add('invisible');
+            }
+        });
+    
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+
+
 });
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
-}
+};
+
